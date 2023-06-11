2fdc032050702093e00a2aa4b6344745da6af801
WangLaoQi <wang.lao.qi@qq.com>
2023-06-11 11:26:41 +0800

    update: add string fiel verify

--- src/middleware/verifyQuery.ts
+++ src/middleware/verifyQuery.ts
@@ -8,9 +8,10 @@ export function verifyQuery(fiel: string, isNull: boolean = false, isArray: bool
 			return
 		}
 
-		if (isArray && Array.isArray(req.query[fiel])) {
+		if (isArray && !Array.isArray(req.query[fiel])) {
+			res.status(400).send({ code: 400, msg: `${fiel} fiel type is not array` })
+		} else if (typeof req.query[fiel] !== 'string') {
 			res.status(400).send({ code: 400, msg: `${fiel} fiel type is not string` })
-			return
 		}
 
 		next()

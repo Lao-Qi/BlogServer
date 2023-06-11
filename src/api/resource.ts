8a2dc03255bf46aeccdaea7e16af47221caf5896
WangLaoQi <wang.lao.qi@qq.com>
2023-06-11 11:25:33 +0800

    update: Enables simple file downloads

--- src/api/resource.ts
+++ src/api/resource.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import multer from 'multer'
 import { unlink } from 'fs/promises'
+import { createReadStream } from 'fs'
 import { Like } from 'typeorm'
-import { join } from 'path'
+import { join } from 'path/win32'
 import { database } from '../database'
 import { Resource } from '../entitys'
 import { verifyToken, verifyQuery, verifyFile } from '../middleware'
@@ -14,10 +15,40 @@ const resourceRepository = database.getRepository(Resource)
 
 resource.get('/', verifyQuery('id'), async (req, res) => {
 	try {
-		const resources = await resourceRepository.findOneBy({ id: parseInt(req.query.id as string) })
-		res.send({ code: 200, msg: 'Search succeed', data: resources ?? {} })
+		const resource = await resourceRepository.findOneBy({ id: parseInt(req.query.id as string) })
+		res.send({ code: 200, msg: 'Search succeed', data: resource ? transformedResource(resource) : {} })
 	} catch (err) {
 		res.status(500).send({ code: 200, msg: 'Search error' })
+	}
+})
+
+resource.get('/getAll', async (_, res) => {
+	try {
+		const resources = await resourceRepository.find()
+		res.send({ code: 200, msg: 'Search succeed', data: resources.map((resource) => transformedResource(resource)) })
+	} catch (err) {
+		res.status(500).send({ code: 200, msg: 'Search error' })
+	}
+})
+
+resource.get('/download', verifyQuery('id'), async (req, res) => {
+	try {
+		const resource = await resourceRepository.findOneBy({ id: Number(req.query.id) })
+		const filePath = join(process.cwd(), uploadPath, resource.filename)
+
+		if (!resource) {
+			res.status(404).send({ code: 404, msg: 'Not found resource' })
+			return
+		}
+
+		resource.downloads += 1
+		await resourceRepository.save(resource)
+
+		res.setHeader('Content-Length', resource.size).setHeader('Content-Type', resource.mimetype).status(200)
+		createReadStream(filePath).pipe(res)
+	} catch (err) {
+		console.error(err)
+		res.status(500).send({ code: 500, msg: 'Download error' })
 	}
 })
 
@@ -29,9 +60,6 @@ resource.get('/search', verifyQuery('key'), async (req, res) => {
 					name: Like(`%${req.query.key}%`)
 				},
 				{
-					decs: Like(`%${req.query.key}%`)
-				},
-				{
 					tags: Like(`%${req.query.key}%`)
 				}
 			],
@@ -41,14 +69,21 @@ resource.get('/search', verifyQuery('key'), async (req, res) => {
 			take: parseInt(req.query.num as string) || 1
 		})
 
-		res.send({ code: 200, msg: 'Search succeed', data: req.query.single === '1' ? resources[0] ?? {} : resources })
+		let data = {}
+		if (req.query.single) {
+			data = resources[0] ? transformedResource(resources[0]) : {}
+		} else {
+			data = resources.map((essay) => transformedResource(essay))
+		}
+
+		res.send({ code: 200, msg: 'Search succeed', data })
 	} catch (err) {
 		console.error(err)
 		res.status(500).send({ code: 500, msg: 'Search error' })
 	}
 })
 
-resource.get('/all', async (_, res) => {
+resource.get('/all', verifyToken(), async (_, res) => {
 	try {
 		const resources = await resourceRepository.find()
 		res.send({ code: 200, msg: 'Search succeed', data: resources })
@@ -58,29 +93,16 @@ resource.get('/all', async (_, res) => {
 	}
 })
 
-resource.get('/download', verifyQuery('id'), async (req, res) => {
-	try {
-		const resource = await resourceRepository.findOneBy({ filename: req.query.id as string })
-		resource.downloads += 1
-		await resourceRepository.save(resource)
-		res.download(join(process.cwd(), uploadPath, resource.filename))
-	} catch (err) {
-		console.error(err)
-		res.status(500).send({ code: 500, msg: 'Download error' })
-	}
-})
-
 resource.post('/upload', verifyToken(), multer({ dest: uploadPath }).any(), verifyFile('resource', uploadPath), async (req, res) => {
-	const resource = new Resource()
-	resource.name = req.body.name ?? req.file.originalname
-	resource.originalname = req.file.originalname
-	resource.setTags(req.body.tags ?? [])
-	resource.decs = req.body.decs ?? ''
-	resource.size = req.file.size
-	resource.filename = req.file.filename
-	resource.mimetype = req.file.mimetype
-
 	try {
+		const resource = new Resource()
+		resource.name = Buffer.from(req.body.name ?? req.file.originalname).toString('utf-8')
+		resource.originalname = Buffer.from(req.file.originalname).toString('utf-8')
+		resource.size = req.file.size
+		resource.filename = req.file.filename
+		resource.mimetype = req.file.mimetype
+		resource.setTags(req.body.tags ? String(req.body.tags).split('--') : [])
+
 		const nresource = await resourceRepository.save(resource)
 		res.send({ code: 200, msg: 'Upload succeed', data: { id: nresource.id, time: nresource.uploadTime, name: nresource.name } })
 	} catch (err) {
@@ -90,7 +112,7 @@ resource.post('/upload', verifyToken(), multer({ dest: uploadPath }).any(), veri
 })
 
 resource.post('/updateFiel', verifyQuery('id'), verifyToken(), async (req, res) => {
-	if (!req.body.name && !req.body.decs && !req.body.tags) {
+	if (!req.body.name && !req.body.tags) {
 		res.status(400).send({ code: 400, msg: 'Update data fiel is null' })
 		return
 	}
@@ -98,7 +120,6 @@ resource.post('/updateFiel', verifyQuery('id'), verifyToken(), async (req, res) 
 	try {
 		const resource = await resourceRepository.findOneBy({ id: parseInt(req.query.id as string) })
 		req.body.name && (resource.name = req.body.name)
-		req.body.decs && (resource.decs = req.body.decs)
 		req.body.tags && resource.setTags(req.body.tags)
 		resource.updateTime = Date.now()
 		const nresource = await resourceRepository.save(resource)
@@ -138,3 +159,15 @@ resource.delete('/remove', verifyQuery('id'), verifyToken(), async (req, res) =>
 		res.status(500).send({ code: 500, msg: `Delete ${req.query.id} data error` })
 	}
 })
+
+function transformedResource(resource: Resource) {
+	return {
+		id: resource.id,
+		name: resource.name,
+		uploadTime: resource.uploadTime,
+		mimeType: resource.mimetype,
+		size: resource.size,
+		tags: resource.tags,
+		originalname: resource.originalname
+	}
+}

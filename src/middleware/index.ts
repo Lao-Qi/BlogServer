e346e347065af0b33ca74307d86419eacf17f214
WangLaoQi <wang.lao.qi@qq.com>
2023-06-16 22:56:08 +0800

    update: export verifyBody middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,5 @@
 export { verifyToken } from './verifyToken'
 export { verifyQuery } from './verifyQuery'
+export { verifyBody } from './verifyBody'
 export { verifyFile } from './verifyFile'
 export { logger } from './logger'

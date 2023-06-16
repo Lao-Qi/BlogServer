c9672a5f9655ae9c6963bc17247718d4db1326fb
WangLaoQi <wang.lao.qi@qq.com>
2023-06-16 22:55:24 +0800

    update: update log time

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express'
 /** 验证File参数 */
 export function logger() {
 	return async (req: Request, res: Response, next: NextFunction) => {
-		console.log(`[${new Date().toLocaleTimeString()}] ${req.ip} ${req.method} ${req.originalUrl}`)
+		console.log(`[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] ${req.ip} ${req.method} ${req.originalUrl}`)
 		next()
 	}
 }
